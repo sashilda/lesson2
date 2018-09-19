92af9ada1772a9a089eefade5c76beab1300bf1a
Alexandra Vlasov <sashilda.vlasov@gmail.com>
2018-09-19 12:01:09 +0300

    try works

--- task_while2.py
+++ task_while2.py
@@ -9,18 +9,27 @@ def ask_user(chat_dictionary):
     c = chat_dictionary
 
     print(" How are you ? ")
+
     while True:
-        
-        user_say = input("Chat with me.   ")
-        user_say = str(user_say)
-        user_say = user_say.capitalize()
-           
-        if user_say == 'Bye!':
+        try:
+            user_say = input("Chat with me.   ")
+            user_say = str(user_say)
+            user_say = user_say.capitalize()
+            
+            if user_say == 'Bye!':
+                print('Well, bye!')
+                break
+            elif user_say in c:
+                print(c.get(user_say)) 
+            else: #if user_say not in c.keys():
+                print("I'm not understand")
+
+        except KeyboardInterrupt as ex:
+
             print('Well, bye!')
             break
-        elif user_say in c:
-            print(c.get(user_say)) 
-        else: #if user_say not in c.keys():
-            print("I'm not understand")
+
+            
+    
 
 ask_user(question_answer)
